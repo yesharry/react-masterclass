f9e0c45d57e6525e880483e72d0f73941a428be7
yesharry <reerohh@gmail.com>
2022-12-06 21:48:10 +0900

    animation practicing

---
+++ src/1-styled-component/AnimationPractice.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+0% {
+  transform: rotate(0deg);
+  border-radius: 0px;
+}
+50% {
+  border-radius:100px;
+}
+100% {
+  transform: rotate(360deg);
+  border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+function AnimationPractice() {
+  return (
+    <Wrapper>
+      <Box>
+        <span>😍</span>
+      </Box>
+    </Wrapper>
+  );
+}
+
+export default AnimationPractice;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StyledPractice from "./1-styled-component/StyledPractice";
+// import StyledPractice from "./1-styled-component/StyledPractice";
+import AnimationPractice from "./1-styled-component/AnimationPractice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StyledPractice />
+    {/* <StyledPractice /> */}
+    <AnimationPractice />
   </React.StrictMode>
 );
