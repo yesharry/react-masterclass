846c092a93a313a4ed719564c40d554a724a6ac4
yesharry <reerohh@gmail.com>
2022-12-07 13:27:09 +0900

    theme practice

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import StyledPractice from "./1-styled-component/StyledPractice";
-import AnimationPseudo from "./1-styled-component/AnimationPseudo";
+// import AnimationPseudo from "./1-styled-component/AnimationPseudo";
+import { ThemeProvider } from "styled-components";
+import ThemePractice from "./1-styled-component/ThemePractice";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <StyledPractice /> */}
-    <AnimationPseudo />
+    {/* 1-styled-component */}
+    <ThemeProvider theme={darkTheme}>
+      <ThemePractice />
+    </ThemeProvider>
+    {/* <StyledPractice />
+      <AnimationPseudo /> */}
   </React.StrictMode>
 );
